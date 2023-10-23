cbd66525da220520464eae51224afa5e1404bb7b
Pranjal <pv2260@gmail.com>
2023-10-23 21:43:54 +0530

    department schema

---
+++ Models/departmentModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const departmentSchema = new mongoose.Schema({
+_id:Number,
+name: {
+    type: String,
+    required: [true, "Name is required"],
+},
+email: {
+    type: String,
+    required: [true, "Please provide email address"],
+    unique: [true, "Email must be unique"],
+    validate: [validator.isEmail, 'Please Enter A valid E-mail']
+},
+contactNumber: {
+    type: Number,
+    required: [true, "Please provide Contact Number"]
+}
+});
+
+module.exports = mongoose.model("departmentModel", departmentSchema );
